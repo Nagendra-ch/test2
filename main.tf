9f41104ad5f98593d3f65f0456d1e598b432354b
Nagendra <nagas400@gmail.com>
2020-10-22 20:28:09 -0400

    updated

--- main.tf
+++ main.tf
@@ -9,6 +9,7 @@ resource "aws_instance" "firstdemo" {
   ami                         = "ami-098f16afa9edf40be"
   instance_type               = "t2.micro"
   vpc_security_group_ids = 	 [aws_security_group.SSH-RDS-Connection.id]
+  vpc_id = aws_vpc.default.id
   subnet_id                   = aws_subnet.first.id
   associate_public_ip_address = true
 
